e21cce41a86396edbfc8a3bfe8db332c080e2675
GA-ENG-TORINO <50743478+GA-ENG-TORINO@users.noreply.github.com>
2020-04-26 20:16:57 +0200

    aaggiunta versione modifica pom

---
+++ webapp/src/main/java/webapp/it/diama/xml/XMLTool.java
@@ -0,0 +1,61 @@
+package webapp.it.diama.xml;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+
+
+public class XMLTool {
+	
+	public Document xml(String path) throws ParserConfigurationException, SAXException, IOException {
+		File fXmlFile = new File(path);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		doc.getDocumentElement().normalize();
+		return doc;
+	}
+	
+	public double readPomVersion(Document doc) {
+		Node verision = getVersion(doc);
+		return Double.valueOf(verision.getTextContent());
+	}
+
+	private Node getVersion(Document doc) {
+		Node nNode = getProgetNode(doc);
+		Element eElement = (Element) nNode;
+		Node verision=eElement.getElementsByTagName("version").item(0);
+		return verision;
+	}
+
+	private Node getProgetNode(Document doc) {
+		Node nNode = (Node) doc.getElementsByTagName("project").item(0);
+		return nNode;
+	}
+	
+	public void rewriteVersion(Document doc,double version,String path) throws TransformerException {
+		Node nNode = getVersion(doc);
+		nNode.setTextContent(String.valueOf(version));
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new File(path));
+		transformer.transform(source, result);
+	}
+
+}
---
+++ webapp/src/test/java/webapp/it/diama/xml/XMLToolTest.java
@@ -0,0 +1,55 @@
+package webapp.it.diama.xml;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLToolTest {
+	private static final String PATH = System.getProperty("user.home")+"/gitRemoto/pom.xml";
+	XMLTool xml = new XMLTool();
+	Document doc;
+	double version;
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@AfterEach
+	protected void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPomVersion() throws Exception {
+		doc=xml.xml(PATH);
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		doc.getChildNodes();
+		Node nNode = (Node) doc.getElementsByTagName("project").item(0);
+		Element eElement = (Element) nNode;
+		eElement.getElementsByTagName("version").item(0).getTextContent();
+				
+	}
+
+	@Test
+	public void testReadPomVersion() throws Exception {
+		testPomVersion();
+		version=xml.readPomVersion(doc);
+		System.out.println(version);
+	}
+
+	@Test
+	public void testRewriteVersion() throws Exception {
+		testReadPomVersion();
+		version=(version*10+1)/10;
+		xml.rewriteVersion(doc, version, PATH);
+		testReadPomVersion();
+	}
+	
+	
+
+}
