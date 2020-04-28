f01462254c4b36d738ab2170a42c4b517f778d2e
GA-ENG-TORINO <50743478+GA-ENG-TORINO@users.noreply.github.com>
2020-04-28 11:53:37 +0200

    refresh page

--- webapp/src/main/webapp/my_jquery.js
+++ webapp/src/main/webapp/my_jquery.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	$.ajax({url: "rest/service/version", success: function(result){
       $("#div1").html(result);
     }});
+	
+	
+	setTimeout(function(){
+		location.reload();
+		}, 5000);
 });
